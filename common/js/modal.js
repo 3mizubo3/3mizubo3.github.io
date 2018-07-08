60a998a792ee29299db832da55823ea04b6c95b5
3mizuho3 <3mizuho3@gmail.com>
2018-07-08 21:46:27 +0900

    ハンバーガーメニューをテキストのホバーをスマホ観覧時にきる

--- common/js/modal.js
+++ common/js/modal.js
@@ -1,4 +1,14 @@
-
+//スマホでみたときのメニューのhoverをやめる処理
+$(function() {
+	if($(window).width() <= 800){
+				$("span.s-balloon").hide();
+		$("#menu-open").on("click",function(){
+				//$("nav.menu").find(".menu-item").find("span").css({"display":"inline-block","opacity":"1"});
+				$("span.s-balloon").toggle();
+		});
+	}
+});
+//----------
 $('.button').click(function(){
   var buttonId = $(this).attr('id');
   $('#modal-container').removeAttr('class').addClass(buttonId);
